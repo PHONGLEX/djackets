6e4a04a417c2f42d1104dfebf4862dbf8b5222c5
phonglex <fonglex@gmail.com>
2021-07-18 11:44:15 +0700

    basic implementation with dockerize

---
+++ django/order/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+from product.models import Product
+
+
+class Order(models.Model):
+
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    paid_amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    stripe_token = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
+
+    def __str__(self):
+        return self.first_name
+
+    def get_absolute_url(self):
+        return reverse("Order_detail", kwargs={"pk": self.pk})
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return '%s' % self.id
---
+++ django/product/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
